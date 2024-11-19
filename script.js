d994f0e9c9fded893f8fda2ebc4dcb5b1274863d
Tyler <144330871+tbperson@users.noreply.github.com>
2024-11-19 16:05:25 +0000

    e

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function update_time() {
     let currentTime = new Date().toLocaleString();
     document.querySelector('#topnav h2').textContent = currentTime;
-    console.log(currentTime);
 }
-
-
 
 //Run the clock every 500ms
 setInterval(update_time, 500);
